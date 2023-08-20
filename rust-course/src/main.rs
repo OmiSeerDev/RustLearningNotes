afd97112255e8984381653646e671b5a50bd6ed1
OmiSeerDev <omarmtinajero@gmail.com>
2023-08-20 01:32:41 -0600

    Starting project, vars, and input by std

---
+++ rust-course/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, Omar! Way to go!");
+    let age: u8 = 28;
+    // int Varibles can be changed to i and a number of bytes
+    // (i8, i16, i32, i64, i128, i256) or u (unsigned).
+
+    // Por defecto las variables son inmutables para mutarlas deben llevar mut
+    let mut times: &str = "years";
+
+     println!("Hi I'm Omar and I'm {} {} old.", times, age);
+
+     times = "centuries";
+     println!("{}" ,times);
+
+     println!("Please insert your name: ");
+     let mut name: String = String::new();
+     std::io::stdin().read_line(&mut name).unwrap();
+
+     println!("Congratulations, {}. You've made your first input log", name);
+}
